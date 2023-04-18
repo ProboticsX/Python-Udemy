0be3e67a39e6104e818546711aff6a962659b799
ProboticsX <jainshubham0403@gmail.com>
2023-04-18 15:09:15 -0400

    Added files

---
+++ section3/blog/tests/unit/post_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from post import Post
+class PostTest(TestCase):
+    def test_create_post(self):
+        p = Post('Title','Content')
+        self.assertEqual('Title',p.title)
+    def test_json(self):
+        p = Post('Title', 'Content')
+        expected = {'title':'Title', 'content':'Content'}
+        self.assertDictEqual(p.json(), expected)
