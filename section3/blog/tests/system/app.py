6d30200182bba43afa4e45ce026d6699f47a3e2a
ProboticsX <jainshubham0403@gmail.com>
2023-04-18 23:15:39 -0400

    Added mock test case for input and call function

--- section3/blog/tests/system/app.py
+++ section3/blog/tests/system/app.py
@@ -1,6 +1,8 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list them, "r" to read one, "p" to write a post, or "q" to quit:'
 blogs = dict() #blog name : blog Object
 def menu():
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
--- section3/blog/tests/system/app_test.py
+++ section3/blog/tests/system/app_test.py
@@ -4,6 +4,15 @@ import app
 from section3.blog.blog import Blog
 
 class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input'): #to have a fake/mock input
+                app.menu()
+                mocked_print_blogs.assert_called()
     def test_print_blogs(self):
         b = Blog('Title', 'Author')
         app.blogs = {b.title:b}
