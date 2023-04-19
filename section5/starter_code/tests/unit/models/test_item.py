cdd04c2d57153eaaa4580fa05256e723064144eb
ProboticsX <jainshubham0403@gmail.com>
2023-04-19 17:45:15 -0400

    Added files and unit tests for section5

---
+++ section5/starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from models.item import ItemModel
+
+class TestItem(TestCase):
+
+    def test_create_item(self):
+        item = ItemModel('test', 19.99)
+        self.assertEqual(item.name, 'test')
+        self.assertEqual(item.price, 19.99)
+
+    def test_item_json(self):
+        item = ItemModel('test', 19.99)
+        expected = {'name':'test', 'price':19.99}
+        self.assertDictEqual(item.json(), expected)
